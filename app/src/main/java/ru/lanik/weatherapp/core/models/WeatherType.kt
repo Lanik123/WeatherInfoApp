6dc1448ad514a894d14a7fa272661894a71457d2
Lanik <daniilt971@gmail.com>
2024-05-19 21:13:53 +0300

    app: core: models: Update weather type structure

--- app/src/main/java/ru/lanik/weatherapp/core/models/WeatherType.kt
+++ app/src/main/java/ru/lanik/weatherapp/core/models/WeatherType.kt
@@ -4,87 +4,115 @@ import androidx.annotation.DrawableRes
 import ru.lanik.weatherapp.R
 
 sealed class WeatherType(
+    val wmo: Int,
     @DrawableRes val iconRes: Int
 ) {
     object ClearSky : WeatherType(
+        wmo = 0,
         iconRes = R.drawable.ic_sunny
     )
     object MainlyClear : WeatherType(
+        wmo = 1,
         iconRes = R.drawable.ic_cloudy
     )
     object PartlyCloudy : WeatherType(
+        wmo = 2,
         iconRes = R.drawable.ic_cloudy
     )
     object Overcast : WeatherType(
+        wmo = 3,
         iconRes = R.drawable.ic_cloudy
     )
     object Foggy : WeatherType(
+        wmo = 45,
         iconRes = R.drawable.ic_very_cloudy
     )
     object DepositingRimeFog : WeatherType(
+        wmo = 48,
         iconRes = R.drawable.ic_very_cloudy
     )
     object LightDrizzle : WeatherType(
+        wmo = 51,
         iconRes = R.drawable.ic_rainshower
     )
     object ModerateDrizzle : WeatherType(
+        wmo = 53,
         iconRes = R.drawable.ic_rainshower
     )
     object DenseDrizzle : WeatherType(
+        wmo = 55,
         iconRes = R.drawable.ic_rainshower
     )
     object LightFreezingDrizzle : WeatherType(
+        wmo = 56,
         iconRes = R.drawable.ic_snowyrainy
     )
     object DenseFreezingDrizzle : WeatherType(
+        wmo = 57,
         iconRes = R.drawable.ic_snowyrainy
     )
     object SlightRain : WeatherType(
+        wmo = 61,
         iconRes = R.drawable.ic_rainy
     )
     object ModerateRain : WeatherType(
+        wmo = 63,
         iconRes = R.drawable.ic_rainy
     )
     object HeavyRain : WeatherType(
+        wmo = 65,
         iconRes = R.drawable.ic_rainy
     )
     object HeavyFreezingRain: WeatherType(
+        wmo = 67,
         iconRes = R.drawable.ic_snowyrainy
     )
     object SlightSnowFall: WeatherType(
+        wmo = 71,
         iconRes = R.drawable.ic_snowy
     )
     object ModerateSnowFall: WeatherType(
+        wmo = 73,
         iconRes = R.drawable.ic_heavysnow
     )
     object HeavySnowFall: WeatherType(
+        wmo = 75,
         iconRes = R.drawable.ic_heavysnow
     )
     object SnowGrains: WeatherType(
+        wmo = 77,
         iconRes = R.drawable.ic_heavysnow
     )
     object SlightRainShowers: WeatherType(
+        wmo = 80,
         iconRes = R.drawable.ic_rainshower
     )
     object ModerateRainShowers: WeatherType(
+        wmo = 81,
         iconRes = R.drawable.ic_rainshower
     )
     object ViolentRainShowers: WeatherType(
+        wmo = 82,
         iconRes = R.drawable.ic_rainshower
     )
     object SlightSnowShowers: WeatherType(
+        wmo = 85,
         iconRes = R.drawable.ic_snowy
     )
     object HeavySnowShowers: WeatherType(
+        wmo = 86,
         iconRes = R.drawable.ic_snowy
     )
     object ModerateThunderstorm: WeatherType(
+        wmo = 95,
         iconRes = R.drawable.ic_thunder
     )
     object SlightHailThunderstorm: WeatherType(
+        wmo = 96,
         iconRes = R.drawable.ic_rainythunder
     )
     object HeavyHailThunderstorm: WeatherType(
+        wmo = 99,
         iconRes = R.drawable.ic_rainythunder
     )
 
